8c5ed6efa66a3fa11f7c9c06ddda45aa9373428b
darkerz7 <25752428+darkerz7@users.noreply.github.com>
2025-06-29 22:21:49 +0500

    Update 1.DZ.3.5
    Latest API

--- EntWatchSharp/EntWatchSharp.cs
+++ EntWatchSharp/EntWatchSharp.cs
@@ -18,7 +18,7 @@ namespace EntWatchSharp
 		public override string ModuleName => "EntWatchSharp";
 		public override string ModuleDescription => "Notify players about entity interactions";
 		public override string ModuleAuthor => "DarkerZ [RUS]";
-		public override string ModuleVersion => "1.DZ.3.4";
+		public override string ModuleVersion => "1.DZ.3.5";
 
 		public override void OnAllPluginsLoaded(bool hotReload)
 		{
