435c0198f9ecf2ac3e37c09f174aff244db97abf
darkerz7 <25752428+darkerz7@users.noreply.github.com>
2026-01-05 17:02:23 +0500

    Update 1.DZ.9.3
    Added compatibility PostgreSQL

--- EntWatchSharp/Modules/Eban/EbanDB.cs
+++ EntWatchSharp/Modules/Eban/EbanDB.cs
@@ -167,7 +167,7 @@ namespace EntWatchSharp.Modules.Eban
 			{
 				Task.Run(() =>
 				{
-					db.AnyDB.QueryAsync("INSERT INTO EntWatch_Current_Eban (client_name, client_steamid, admin_name, admin_steamid, server, duration, timestamp_issued, reason) VALUES ({ARG}, {ARG}, {ARG}, {ARG}, {ARG}, {ARG}, {ARG}, {ARG});", new List<string>([sClientName, sClientSteamID, sAdminName, sAdminSteamID, sServer, iDuration.ToString(), iTimeStamp.ToString(), sReason]), (_) => { }, true, true);
+					db.AnyDB.QueryAsync("INSERT INTO EntWatch_Current_Eban (client_name, client_steamid, admin_name, admin_steamid, server, duration, timestamp_issued, reason) VALUES ({ARG}, {ARG}, {ARG}, {ARG}, {ARG}, {ARG}::int, {ARG}::int, {ARG});", new List<string>([sClientName, sClientSteamID, sAdminName, sAdminSteamID, sServer, iDuration.ToString(), iTimeStamp.ToString(), sReason]), (_) => { }, true, true);
 				});
 			}
 		}
@@ -179,7 +179,7 @@ namespace EntWatchSharp.Modules.Eban
 				Task.Run(() =>
 				{
 					if (Cvar.KeepExpiredBan)
-						db.AnyDB.QueryAsync("UPDATE EntWatch_Current_Eban SET reason_unban = {ARG}, admin_name_unban = {ARG}, admin_steamid_unban = {ARG}, timestamp_unban = {ARG} " +
+						db.AnyDB.QueryAsync("UPDATE EntWatch_Current_Eban SET reason_unban = {ARG}, admin_name_unban = {ARG}, admin_steamid_unban = {ARG}, timestamp_unban = {ARG}::int " +
 												"WHERE client_steamid={ARG} and server={ARG} and admin_steamid_unban IS NULL;" +
 											"INSERT INTO EntWatch_Old_Eban (client_name, client_steamid, admin_name, admin_steamid, server, duration, timestamp_issued, reason, reason_unban, admin_name_unban, admin_steamid_unban, timestamp_unban) " +
 												"SELECT client_name, client_steamid, admin_name, admin_steamid, server, duration, timestamp_issued, reason, reason_unban, admin_name_unban, admin_steamid_unban, timestamp_unban FROM EntWatch_Current_Eban " +
@@ -252,7 +252,7 @@ namespace EntWatchSharp.Modules.Eban
 				if (Cvar.KeepExpiredBan)
 					{
 						db.AnyDB.QueryAsync("SELECT id FROM EntWatch_Current_Eban " +
-									"WHERE server={ARG} and duration>0 and timestamp_issued<{ARG};", new List<string>([sServer, iTime.ToString()]), (res) =>
+                                    "WHERE server={ARG} and duration>0 and timestamp_issued<{ARG}::int;", new List<string>([sServer, iTime.ToString()]), (res) =>
 						{
 							if (res.Count > 0)
 							{
@@ -262,17 +262,17 @@ namespace EntWatchSharp.Modules.Eban
 									sIDs = sIDs + ", " + res[i][0];
 								}
 								sIDs = sIDs[2..];
-								db.AnyDB.QueryAsync("UPDATE EntWatch_Current_Eban SET reason_unban='Expired', admin_name_unban='Console', admin_steamid_unban='SERVER', timestamp_unban={ARG} WHERE id IN ({ARG});" +
+								db.AnyDB.QueryAsync("UPDATE EntWatch_Current_Eban SET reason_unban='Expired', admin_name_unban='Console', admin_steamid_unban='SERVER', timestamp_unban={ARG}::int WHERE id IN ({ARG}::int);" +
 									"INSERT INTO EntWatch_Old_Eban(client_name, client_steamid, admin_name, admin_steamid, server, duration, timestamp_issued, reason, reason_unban, admin_name_unban, admin_steamid_unban, timestamp_unban) " +
-										"SELECT client_name, client_steamid, admin_name, admin_steamid, server, duration, timestamp_issued, reason, reason_unban, admin_name_unban, admin_steamid_unban, timestamp_unban FROM EntWatch_Current_Eban WHERE id IN ({ARG});" +
-									"DELETE FROM EntWatch_Current_Eban WHERE id IN ({ARG});", new List<string>([iTime.ToString(), sIDs, sIDs, sIDs]), (_) => { }, true);
+                                        "SELECT client_name, client_steamid, admin_name, admin_steamid, server, duration, timestamp_issued, reason, reason_unban, admin_name_unban, admin_steamid_unban, timestamp_unban FROM EntWatch_Current_Eban WHERE id IN ({ARG}::int);" +
+                                    "DELETE FROM EntWatch_Current_Eban WHERE id IN ({ARG}::int);", new List<string>([iTime.ToString(), sIDs, sIDs, sIDs]), (_) => { }, true);
 							}
 						});
 					}
 					else
 					{
 						db.AnyDB.QueryAsync("DELETE FROM EntWatch_Current_Eban WHERE id IN (SELECT p.id FROM (" +
-								"SELECT id FROM EntWatch_Current_Eban WHERE server={ARG} and duration>0 and timestamp_issued<{ARG}) AS p);", new List<string>([sServer, iTime.ToString()]), (_) => { }, true);
+                                "SELECT id FROM EntWatch_Current_Eban WHERE server={ARG} and duration>0 and timestamp_issued<{ARG}::int) AS p);", new List<string>([sServer, iTime.ToString()]), (_) => { }, true);
 					}
 				});
 			}
